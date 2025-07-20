fa877c73366a38896fbcd661f9a2143b36560749
bryanmsombra27 <bryanjm96@gmail.com>
2025-07-20 13:43:45 -0600

    detail post dynamic part added !

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from datetime import date
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -69,14 +69,28 @@ posts = [
 
 # Create your views here.
 
+def get_date(post):
+    return post['date']
+
 
 def index(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latests_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latests_posts
+    })
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 
 def post(request, slug):
-    return render(request, "blog/post-detail.html")
+    single_post = next(post for post in all_posts if post["slug"] == slug)
+
+    return render(request, "blog/post-detail.html", {
+        "post": single_post
+    })
