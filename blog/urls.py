12697f79b4459646da330ddf388ae0bc202e4d85
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-17 13:18:03 -0600

    refactoring image path and file upload

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post, name="post-detail-page"),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
