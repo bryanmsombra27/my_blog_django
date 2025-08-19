9a98bc4b3f48b50a10fd117e2e5d864240fb5e55
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-19 10:23:27 -0600

    working on comments functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post, name="post-detail-page"),
+    path("", views.StartingPageView.as_view(), name="index"),
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.PostDetailView.as_view(),
+         name="post-detail-page"),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,104 +1,54 @@
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post
-# all_posts = [
-#     {
-#         "slug": "hike-in-the-mountains",
-#         "image": "mountains.jpg",
-#         "author": "Maximilian",
-#         "date": date(2021, 7, 21),
-#         "title": "Mountain Hiking",
-#         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     },
-#     {
-#         "slug": "programming-is-fun",
-#         "image": "coding.jpg",
-#         "author": "Maximilian",
-#         "date": date(2022, 3, 10),
-#         "title": "Programming Is Great!",
-#         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     },
-#     {
-#         "slug": "into-the-woods",
-#         "image": "woods.jpg",
-#         "author": "Maximilian",
-#         "date": date(2020, 8, 5),
-#         "title": "Nature At Its Best",
-#         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     }
-# ]
+from .forms import CommentForm
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
-# Create your views here.
+class StartingPageView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ["-date"]
 
-def get_date(post):
-    return post['date']
-
-
-def index(request):
-    all_posts = Post.objects.all().order_by("-date")[:3]
-
-    # sorted_posts = sorted(all_posts, key=get_date)
-    # latests_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        # "posts": latests_posts
-        "posts": all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
+class AllPostsView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    context_object_name = "all_posts"
+    ordering = ["-date"]
 
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
 
+class PostDetailView(View):
 
-def post(request, slug):
-    # single_post = next(post for post in all_posts if post["slug"] == slug)
-    # post = Post.objects.get(slug=slug)
-    post = get_object_or_404(Post, slug=slug)
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        return render(request, "blog/post-detail.html", {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": CommentForm(),
+        })
 
-    return render(request, "blog/post-detail.html", {
-        "post": post,
-        "tags": post.tags.all()
-    })
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        return render(request, "blog/post-detail.html", {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": comment_form,
+        })
