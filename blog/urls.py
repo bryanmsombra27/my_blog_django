a5ef24d873af16818e55bdf047941100bc15c9f1
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-20 20:07:15 -0600

    adding gitignore file

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="index"),
@@ -10,4 +9,4 @@ urlpatterns = [
          name="post-detail-page"),
     path("read-later", views.ReadLaterView.as_view(), name="read-later")
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- my_site/settings.py
+++ my_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os import getenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fz5^v0$f!9=g*2yb%4doxmuf0(@m8=(vh61jupv=xq$_-9ejmj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
@@ -117,6 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 
